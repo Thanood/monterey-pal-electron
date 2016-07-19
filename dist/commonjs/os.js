4d1dab66d1e7b14e7518d2b30f48ddcf85449641
Jeroen <Jeroenvinke17@gmail.com>
2016-07-19 12:07:53 +0200

    feat(OS): expose versions of node/npm/chrome/electron

--- dist/commonjs/os.js
+++ dist/commonjs/os.js
@@ -7,6 +7,7 @@ Object.defineProperty(exports, "__esModule", {
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var os = System._nodeRequire('os');
+var child_process = System._nodeRequire('child_process');
 
 var OS = exports.OS = function () {
   function OS() {
@@ -15,6 +16,23 @@ var OS = exports.OS = function () {
 
   OS.prototype.getPlatform = function getPlatform() {
     return os.platform();
+  };
+
+  OS.prototype.getNodeVersion = function getNodeVersion() {
+    return process.versions.node;
+  };
+
+  OS.prototype.getNPMVersion = function getNPMVersion() {
+    var npm = System._nodeRequire('npm');
+    return npm.version;
+  };
+
+  OS.prototype.getChromeVersion = function getChromeVersion() {
+    return process.versions.chrome;
+  };
+
+  OS.prototype.getElectronVersion = function getElectronVersion() {
+    return process.versions.electron;
   };
 
   return OS;
--- src/os.js
+++ src/os.js
@@ -1,7 +1,25 @@
 const os = System._nodeRequire('os');
+let child_process = System._nodeRequire('child_process');
 
 export class OS {
   getPlatform() {
     return os.platform();
+  }
+
+  getNodeVersion() {
+    return process.versions.node;
+  }
+
+  getNPMVersion() {
+    const npm = System._nodeRequire('npm');
+    return npm.version;
+  }
+
+  getChromeVersion() {
+    return process.versions.chrome;
+  }
+
+  getElectronVersion() {
+    return process.versions.electron;
   }
 }
