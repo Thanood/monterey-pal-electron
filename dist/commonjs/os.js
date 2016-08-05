de9294642daaacf9ad3a049e4b2fd77da7fc4865
Jeroen <Jeroenvinke17@gmail.com>
2016-08-05 11:16:44 +0200

    feat(OS): added spawn, exec and kill

--- dist/commonjs/os.js
+++ dist/commonjs/os.js
@@ -8,6 +8,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var os = System._nodeRequire('os');
 var child_process = System._nodeRequire('child_process');
+var treeKill = System._nodeRequire('tree-kill');
 
 var OS = exports.OS = function () {
   function OS() {
@@ -33,6 +34,47 @@ var OS = exports.OS = function () {
 
   OS.prototype.getElectronVersion = function getElectronVersion() {
     return process.versions.electron;
+  };
+
+  OS.prototype.spawn = function spawn(cmd, args, options, stdout, stderr) {
+    var proc = child_process.spawn(cmd, args, options);
+    var resolve = void 0;
+    var promise = new Promise(function (r) {
+      return resolve = r;
+    });
+
+    proc.stdout.on('data', function (data) {
+      stderr(data.toString());
+    });
+
+    proc.stderr.on('data', function (data) {
+      stderr(data.toString());
+    });
+
+    proc.on('exit', function (code) {
+      resolve(code);
+    });
+
+    return {
+      process: proc,
+      completion: promise
+    };
+  };
+
+  OS.prototype.kill = function kill(process) {
+    treeKill(process.pid, 'SIGKILL');
+  };
+
+  OS.prototype.exec = function exec(cmd, options) {
+    return new Promise(function (resolve, reject) {
+      child_process.exec(cmd, options, function (error, stdout, stderr) {
+        if (stdout) {
+          resolve(stdout);
+        } else {
+          reject(error);
+        }
+      });
+    });
   };
 
   return OS;
--- src/os.js
+++ src/os.js
@@ -1,5 +1,6 @@
 const os = System._nodeRequire('os');
 let child_process = System._nodeRequire('child_process');
+let treeKill = System._nodeRequire('tree-kill');
 
 export class OS {
   getPlatform() {
@@ -21,5 +22,46 @@ export class OS {
 
   getElectronVersion() {
     return process.versions.electron;
+  }
+
+  // executes command, streams stdout + stderr
+  spawn(cmd, args, options, stdout, stderr) {
+    let proc = child_process.spawn(cmd, args, options);
+    let resolve;
+    let promise = new Promise(r => resolve = r);
+
+    proc.stdout.on('data', (data) => {
+      stderr(data.toString());
+    });
+
+    proc.stderr.on('data', (data) => {
+      stderr(data.toString());
+    });
+
+    proc.on('exit', (code) => {
+      resolve(code);
+    });
+
+    return {
+      process: proc,
+      completion: promise
+    };
+  }
+
+  kill(process) {
+    treeKill(process.pid, 'SIGKILL');
+  }
+
+  // execute command, resolve when everything has been written to stdout / stderr
+  exec(cmd, options) {
+    return new Promise((resolve, reject) => {
+      child_process.exec(cmd, options, (error, stdout, stderr) => {
+        if (stdout) {
+          resolve(stdout);
+        } else {
+          reject(error);
+        }
+      });
+    });
   }
 }
